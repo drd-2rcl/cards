5e20e5f2e33d36d19c9bd3f48b67c17cfeafe0f2
drd-2rcl <drd.rclrd@gmail.com>
2020-03-08 13:51:44 -0300

    Add conditional to ok or error in load cards

--- lib/cards.ex
+++ lib/cards.ex
@@ -28,6 +28,10 @@ defmodule Cards do
   @spec load(any) :: any
   def load(filename) do
     {status, binary} = File.read(filename)
-    :erlang.binary_to_term binary
+
+    case status do
+      :ok -> :erlang.binary_to_term binary
+      :error -> "That file does not exist"
+    end
   end
 end
