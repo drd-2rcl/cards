d2cd011db675c298ec6fd4510595c8d148020c05
Diogo <drd.rclrd@gmail.com>
2020-03-22 20:05:46 -0300

    Add descrition to documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,8 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creatind and handling a deck of cards
+  """
+
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
